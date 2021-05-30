798b7660a87d410faa3fe2edc5c2018178d2dca9
Vishnu Sivan <codemaker2015@gmail.com>
2021-05-30 19:47:08 +0530

    Initial commit

---
+++ Assets/Scripts/NavMeshPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPlayer : MonoBehaviour
+{
+
+    GameObject destination;
+	NavMeshAgent navMeshAgent;
+ 
+	// Start is called before the first frame update
+	void Start () {
+		destination = GameObject.Find ("Destination");
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.SetDestination(destination.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# NavMesh Agent Demo
+NavMesh agent demo app in Unity 3D
+
+https://codemaker2015.medium.com/navmesh-agent-implementation-in-unity-3d-b136401d027f
